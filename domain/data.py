c17abe055ccd6339d480530360a3f37230641ce3
ofelipp <ofelippm@gmail.com>
2023-12-27 17:22:02 -0300

    [ADD] create Features and Forecast Data Class

--- domain/data.py
+++ domain/data.py
@@ -1,12 +1,50 @@
-from abc import ABCMeta
 from dataclasses import dataclass, field
 import numpy as np
+
+from domain.station import Station
+
+ORDER_FEATURES = [
+    "temperature_C", "cloud_cover_percentual", "altitude_m"
+]
 
 
 @dataclass
 class Data:
-    __metadata__ = ABCMeta
-
     data: np.array
     name: str = field(default=None)
-    details: str = field(default=None)
+
+
+@dataclass
+class Features(Data):
+    hour: str = field(default=None)
+    station: Station = field(default=None)
+    features_names: list = field(default_factory=ORDER_FEATURES)
+
+
+@dataclass
+class Forecast(Data):
+    data: dict
+    station: Station = field(default=None)
+
+    def as_dict(self):
+        return {
+            "station": self.station.name,
+            "coordinates": self.station.coordinates.as_dict(),
+            "forecast": {
+                f"hour_{hour}": forecast
+                for hour, forecast in self.data.items()
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
